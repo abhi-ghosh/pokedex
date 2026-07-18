c12f291cbaa8c00888f4f3575e812588cc2b4b65
Abhijit Ghosh <a7ghosh@gmail.com>
2026-07-18 23:41:50 +0530

    Separated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import searchPokemon from './services/pokeapi';
+import Searchbar from './components/Searchbar';
 function App() {
   const [pokemon, setPokemon] = useState(null);
   const [pokemonData, setPokemonData] = useState(null);
@@ -20,27 +21,7 @@ function App() {
   }
 return (
   <div className="App">
-    <h1 className="text-red-600">Pokedex</h1>
-    <h1>Pokemon Search</h1>
-    <div className="search-container">
-      <input
-        type="text"
-        placeholder="Enter Pokemon name"
-        onChange={(e) => setPokemon(e.target.value)}
-        onKeyDown={(e) => e.key === 'Enter' && handleSearch(pokemon)}/>
-      <button onClick={() => handleSearch(pokemon)}>Search</button>
-      {loading ? (
-        <div className="spinner-container">
-          <div className="spinner-border text-success" role="status"></div>
-        </div>
-      ) :pokemonData && pokemonData.sprites ? (
-        <img
-          src={pokemonData.sprites.front_default}
-          alt={pokemonData.name}
-        />
-      ): error ? (<img src='assets/joy.jpg'/>) : null}
-    </div>
-    {error && <p>Sorry! No record found :(</p>}
+    <Idle setPokemon={setPokemon} handleSearch={handleSearch} pokemon={pokemon}/>
   </div>
 )
 }
