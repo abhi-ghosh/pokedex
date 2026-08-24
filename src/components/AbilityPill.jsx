07758a8e0cc74be2713be9a68cd4fb0f3f86b40c
Abhijit Ghosh <a7ghosh@gmail.com>
2026-08-24 15:40:14 +0530

    feat: added useful comments to code

--- src/components/AbilityPill.jsx
+++ src/components/AbilityPill.jsx
@@ -1,20 +1,26 @@
 export default function AbilityPill({number, name, description, hidden, borderColor, backgroundColor, color}){
   return (
+    //* Ability pill div
     <div className="text-off-white flex gap-2 px-2 py-3  border-l-[5px] rounded-2xl"
       style={hidden ? {borderColor:"#ff00a2", backgroundColor: `#ff00a2${10}`} :
         {borderColor:borderColor ,backgroundColor: backgroundColor}}
     >
+      {/* //* Ability pill number */}
       <p className="flex justify-center items-center w-min text-black
         leading-none font-mono font-bold text-2xl p-2  dark:text-off-white"
       >
         {number}
       </p>
+      {/* //* Name, Hidden & Description div */}
       <div className="flex flex-col flex-1 gap-1"
       >
+        {/* //* Name & Hidden div */}
         <div className="flex flex-row justify-between items-center pr-2">
+          {/* //* Name */}
           <p className="font-mono text-lg font-bold text-gray-500 dark:text-gray-400">
             {name}
           </p>
+          {/* //* Hidden */}
           <p className="flex justify-center items-center w-min text-sm
             leading-none font-outfit font-bold p-2 border-x-2 rounded-xl"
             style={hidden ? {color: "#ff00a2"} :
@@ -23,6 +29,7 @@ export default function AbilityPill({number, name, description, hidden, borderCo
             {hidden ? "HIDDEN" : "NORMAL"}
           </p>
         </div>
+        {/* //* Description */}
         <p className="text-sm dark:text-off-white text-black">{description}</p>
       </div>
     </div>
